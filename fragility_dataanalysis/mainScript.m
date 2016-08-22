ce05a3cc85bfbdfcedf8c41e345305023e7bd39f
Adam <adam2392@gmail.com>
2016-08-22 15:38:42 -0400

    Adding the class implementation of ista and fista. Not completed yet, but simulations work.

--- fragility_dataanalysis/mainScript.m
+++ fragility_dataanalysis/mainScript.m
@@ -8,6 +8,10 @@ patient_file_path = './data/pt1sz2/';
 frequency_sampling = 1000; % sampling freq. at 1 kHz
 BP_FILTER_RAW = 1;
 
+bands = [0 4; 4 8; 8 13; 13 30; 30 90];
+gamma = bands(5, :);
+
+
 % For more information, see the TEXTSCAN documentation.
 formatSpec = '%s%{MM/dd/yyyy}D%{HH:mm:ss}D%{HH:mm:ss}D%{HH:mm:ss}D%f%f%s%[^\n\r]';
 
@@ -52,7 +56,7 @@ end
 %% READ EEG FILE
 % files to process
 f = dir([patient_file_path '*eeg.csv']);
-patient_file_names = cell(1, length(f));
+patient_file_names = cell(1, length(f))
 for i=1:length(f)
     patient_file_names{i} = f(i).name;
 end
@@ -65,22 +69,72 @@ disp(['Number of channels ', num2str(num_channels)]);
 filename = patient_file_names{1};
 num_values = patient_files(patient_file_names{1});
 
-% 1A. extract eeg and apply band notch filter between 59.5 - 60.5
+% 1A. extract eeg 
 eeg = csv2eeg(patient_file_path, filename, num_values, num_channels);
+
+% 1B. apply band notch filter
 eeg = buttfilt(eeg,[59.5 60.5], frequency_sampling,'stop',1); %-filter is overkill: order 1 --> 25 dB drop (removing 5-15dB peak)
 
-% 1B. pre-process channel data by normalization at each time point
+% 1C. pre-process channel data by normalization at each time point
 eeg = eeg - repmat(mean(eeg, 1), size(eeg, 1), 1);
 eeg = eeg ./ repmat(std(eeg, [], 1), size(eeg, 1), 1);
 
 % 1C. only get columns of interest and time points of interest
-timeSStart = seconds(onset_time - recording_start) * frequency_sampling;
-timeWindow = [timeSStart - 20*frequency_sampling: timeSStart - 10*frequency_sampling];
-winSize = 10*frequency_sampling;
+timeSStart = seconds(onset_time - recording_start) * frequency_sampling; % time seizure starts
+file_length = length(eeg);
+num_channels = length(included_channels);
 
-eeg = eeg(included_channels, timeWindow);
+% window parameters - overlap, #samples, stepsize, window pointer
+sliding_window_overlap = 0.5;                                            % window overlap (seconds)
+nsamples = round(sliding_window_overlap * frequency_sampling);           % number of samples to analyze
+stepwin = 1;                                                             % step size of sliding horizon (seconds)
+lastwindow = timeSStart - 10;                                                 % where to grab data
+sample_to_access = lastwindow;
 
-size(eeg)
-bands = [0 4; 4 8; 8 13; 13 30; 30 90];
-gamma = bands(5, :);
+tic;
+limit = fix((file_length - (nsamples - stepwin * frequency_sampling)) / (stepwin * frequency_sampling));
+limit = timeSStart / frequency_sampling;
 
+while (sample_to_access < limit)
+    % step 1: extract the data and apply the notch filter. Note that column
+    %         #i in the extracted matrix is filled by data samples from the
+    %         recording channel #i.
+    tmpdata = eeg(included_channels, lastwindow + 1:lastwindow + nsamples);
+
+    size(tmpdata)
+
+    % step 2: compute some functional connectivity 
+    % linear model: Ax = b; A\b -> x
+    b = tmpdata(:); b = b(num_channels+1:end); % define b 
+    A = zeros(length(b), num_channels^2);
+    A(1, 1:num_channels) = tmpdata(:, 1);      % build up A matrix
+    pointerA = num_channels;
+    tic; % takes around 481 seconds to build the matrix
+    for i=2:size(A, 1)-1
+        pointerA = (i-1)*num_channels;
+        try
+            A(i, pointerA+1:pointerA+num_channels) = tmpdata(:, i);
+        catch
+            pointerA+1
+            pointerA+num_channels;
+        end
+    end
+    toc;
+    size(A)
+    
+    tic;
+    theta = A\b;                                % solve for x, connectivity
+    toc;
+    
+%     R = corrcoef(tmpdata');
+%     outputfile = sprintf('theta_mat_%s.mat', num2str(sample_to_access));
+%     save(outputfile, 'theta');
+
+%     %%- Do some check on the eigenspectrum of the connectivity matrix
+    E = eig(theta);
+    
+    % step 3: update the pointer and window
+    sample_to_access = sample_to_access + stepwin;
+    lastwindow = frequency_sampling * sample_to_access;
+end
+    
