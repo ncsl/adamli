f7d584bc6e6c4fe15cba646c7aba4b4e13827d0b
Adam <adam2392@gmail.com>
2016-09-13 16:14:28 -0400

    Updated main script for the generation of adj. matrices.

--- fragility_dataanalysis/mainScript.m
+++ fragility_dataanalysis/mainScript.m
@@ -2,15 +2,29 @@
 % x = Ax
 %% 0: READ PATIENT ID FILE
 % change these paramters depending on patient
-pat_id = 'pt2'; sz_id = 'sz1';
+pat_id = 'pt2'; sz_id = 'sz3';
 %pat_id = 'JH105'; sz_id = 'sz1';
 
-included_channels = [1:36 42 43 46:54 56:69 72:95]; % pt1
-% included_channels = [1:3 5:22 24:37 42 43 46:85 88 89]; %pt10
-included_channels = [1:4 7:12 14:19 21:37 42 43 46:49 51:53 55:75 78:99]; %JH105
-included_channels = [1:19 21:37 43 44 47:74 75 79]; %pt2
-
-patient = strcat(pat_id, sz_id);
+if strcmp(pat_id, 'pt1')
+    included_channels = [1:36 42 43 46:69 72:95];
+    ezone_labels = {'POLPST1', 'POLPST2', 'POLPST3', 'POLAD1', 'POLAD2'}; %pt1
+    ezone_labels = {'POLATT1', 'POLATT2', 'POLAD1', 'POLAD2', 'POLAD3'}; %pt1
+    earlyspread_labels = {'POLATT3', 'POLAST1', 'POLAST2'};
+    latespread_labels = {'POLATT4', 'POLATT5', 'POLATT6', ...
+                        'POLSLT2', 'POLSLT3', 'POLSLT4', ...
+                        'POLMLT2', 'POLMLT3', 'POLMLT4', 'POLG8', 'POLG16'};
+elseif strcmp(pat_id, 'pt2')
+    included_channels = [1:19 21:37 43 44 47:74 75 79]; %pt2
+    ezone_labels = {'POLMST1', 'POLPST1', 'POLTT1'}; %pt2
+    earlyspread_labels = {'POLTT2', 'POLAST2', 'POLMST2', 'POLPST2', 'POLALEX1', 'POLALEX5'};
+elseif strcmp(pat_id, 'JH105')
+    included_channels = [1:4 7:12 14:19 21:37 42 43 46:49 51:53 55:75 78:99]; % JH105
+    ezone_labels = {'POLRPG4', 'POLRPG5', 'POLRPG6', 'POLRPG12', 'POLRPG13', 'POLG14',...
+        'POLAPD1', 'POLAPD2', 'POLAPD3', 'POLAPD4', 'POLAPD5', 'POLAPD6', 'POLAPD7', 'POLAPD8', ...
+        'POLPPD1', 'POLPPD2', 'POLPPD3', 'POLPPD4', 'POLPPD5', 'POLPPD6', 'POLPPD7', 'POLPPD8', ...
+        'POLASI3', 'POLPSI5', 'POLPSI6', 'POLPDI2'}; % JH105
+end
+patient = strcat(pat_id, sz_id)
 patfile = strcat(patient, '.csv');
 patient_eeg_path = strcat('./data/', patient);
 
