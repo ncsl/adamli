dd05bc26184a2d2f208f753339ce044179253e4b
Adam Li <adam2392@gmail.com>
2016-09-27 23:26:52 -0400

    Adding functions to run parallelized code on icm server.

---
+++ fragility_dataanalysis/leastSquaresAdjMat.m
@@ -0,0 +1,66 @@
+% function: leastSquaresAdjMat
+% Description: Used to compute an adjacency matrix using least squares
+% solving of a linear system of equations of a window of ecog data.
+% 
+% Input:
+% 
+function leastSquaresAdjMat(i, eeg, included_channels, patient, ...
+         winSize, stepSize, ezone_labels, earlyspread_labels, latespread_labels)
+    
+    dataWindow = dataStart + (i-1)*stepSize;
+    
+    % step 1: extract the data and apply the notch filter. Note that column
+    %         #i in the extracted matrix is filled by data samples from the
+    %         recording channel #i.
+    tmpdata = eeg(included_channels, dataWindow + 1:dataWindow + winSize);
+
+    % step 2: compute some functional connectivity 
+    % linear model: Ax = b; A\b -> x
+    b = tmpdata(:); % define b as vectorized by stacking columns on top of another
+    b = b(num_channels+1:end); % only get the time points after the first one
+    
+    tmpdata = tmpdata';
+    tic;
+    % build up A matrix with a loop modifying #time_samples points and #chans at a time
+    A = zeros(length(b), num_channels^2);               % initialize A for speed
+    N = 1:num_channels:size(A,1);                       % set the indices through rows
+    A(n, 1:num_channels) = tmpdata(1:end-1,:);          % set the first loop
+    
+    for iChan=2 : num_channels % loop through columns #channels per loop
+        rowInds = n+(iChan-1);
+        colInds = (iChan-1)*num_channels+1:iChan*num_channels;
+        A(rowInds, colInds) = tmpdata(1:end-1,:);
+    end
+    toc;
+    
+    % create the reshaped adjacency matrix
+    tic;
+    theta = A\b;                                                % solve for x, connectivity
+    theta_adj = reshape(theta, num_channels, num_channels)';    % reshape fills in columns first, so must transpose
+    imagesc(theta_adj)
+    toc;
+    
+    %% save the theta_adj made
+    fileName = strcat(patient, '_', num2str(index), '.mat');
+    
+    %- save the data into a struct into a mat file
+    %- save the data into a struct into a mat file - time all in
+    %milliseconds
+    data = struct();
+    data.theta_adj = theta_adj;
+    data.seizureTime = seizureStart;
+    data.seizureEnd = seizureEnd;
+    data.winSize = winSize;
+    data.stepSize = stepSize;
+    data.timewrtSz = dataWindow - seizureStart;
+    data.timeStart = seizureStart - preseizureTime*frequency_sampling;
+    data.timeEnd = seizureStart + postseizureTime*frequency_sampling;
+    data.index = i;
+    data.included_channels = included_channels;
+    data.ezone_labels = ezone_labels;
+    data.earlyspread_labels = earlyspread_labels;
+    data.latespread_labels = latespread_labels;
+    data.date = date;
+    
+    save(fullfile(adjDir, fileName), 'data');
+end
--- fragility_dataanalysis/runAnalysis.sh
+++ fragility_dataanalysis/runAnalysis.sh
@@ -1,20 +1,19 @@
-#!/Documents/adamli/fragility_dataanalysis/bash
 # This script runs the computation/estimation of adjacency matrices
 
 clear # clear terminal window
 
 echo "Begin estimation of adjacency matrices."
-cd $PBS_O_WORKDIR				# go to the working directory
+# cd $PBS_O_WORKDIR				# go to the working directory
 
 # set parameters for matlab to run, and check if matlab is on path
-matlab_jvm = "matlab -nojvm -nodesktop -nosplash -r"
+matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
 [[ ! -z "`which matlab`" ]] || \
 	{ 
 		echo "MATLAB not found on the PATH; please add to path."; 
 		exit 1;
 	}
 
-$matlab_jvm "mainScript; exit"
+$matlab_jvm "computeAdjMats; dataWindow exit"
 
 
 # winSize = 500
