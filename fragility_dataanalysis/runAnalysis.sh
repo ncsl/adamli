9fb6d123a58fcca2b0a808fcfc3beac53df42ddb
Adam <adam2392@gmail.com>
2016-09-29 14:45:05 -0400

    Making the server directory of running data.

--- fragility_dataanalysis/runAnalysis.sh
+++ fragility_dataanalysis/runAnalysis.sh
@@ -19,7 +19,9 @@ matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
 ## 02: Run matlab job for each window
 for index in `seq 0 5`
 do
-	index=($proc+1)+24*job # set index for the window
+	index=$((proc+1))
+	jobnum=$((24\*job)) # set index for the window
+	index=$((index+jobnum))
 
 	# open matlab and call functions
 	matlab -logfile /home/ali/adamli/fragility_dataanalysis/_log/job$1.txt -nojvm \ 
