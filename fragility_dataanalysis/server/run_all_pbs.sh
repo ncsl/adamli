522f839179b1d68030517b6c97b9ec2bb06e62c8
Adam Li <adam2392@gmail.com>
2016-09-29 16:02:41 -0400

    Updating main job.

--- fragility_dataanalysis/server/run_all_pbs.sh
+++ fragility_dataanalysis/server/run_all_pbs.sh
@@ -5,7 +5,3 @@ chmod -R 777 .
 qsub -l walltime=24:00:00,nodes=node054 run_b_sleep.sh
 qsub -l walltime=24:00:00,nodes=node215 run_b_sleep.sh
 qsub -l walltime=24:00:00,nodes=node232 run_b_sleep.sh
-
-for i in `find ./ -name '*.pbs'` ; 
-do qsub $i ;  
-done
