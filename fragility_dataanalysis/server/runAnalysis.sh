185b3816b5efc2e725cba1838bb6f432aa8e8ca0
Adam <adam2392@gmail.com>
2016-09-29 15:47:11 -0400

    Adding all changes necessary to run on server.

---
+++ fragility_dataanalysis/server/runAnalysis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+# This script runs the computation/estimation of adjacency matrices
+source /etc/profile.modules
+module load matlab/matlab2013a
+
+## 00: Load in input parameters
+proc="$1"
+patient_id="$2"
+seizure_id="$3"
+perturbationType="$4" 
+
+# echo $proc
+# echo $patient_id
+# echo $seizure_id
+# echo $perturbationType
+
+## 01: Set parameters for matlab to run, and check if matlab is on path
+matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
+[[ ! -z "`which matlab`" ]] || \
+	{ 
+		echo "MATLAB not found on the PATH; please add to path."; 
+		exit 1;
+	}
+## 02: Run matlab job for each window
+for ind in `seq 0 0`
+do
+	winIndex=$(($proc+1))
+	job=$((24*ind))
+	winIndex=$((index+job))
+	echo $winIndex
+
+	# open matlab and call functions
+	matlab -logfile /home/ali/adamli/fragility_dataanalysis/_log/job$1.txt -nojvm -nodisplay -nosplash -r "patient_id='$patient_id'; \
+ 			seizure_id='$seizure_id'; \
+ 			serverEZTMainScript; \
+ 			serverLeastSquaresAdjMat($winIndex, eeg, included_channels, patient, \
+	          winSize, stepSize, ezone_labels, earlyspread_labels, latespread_labels); exit"
+done
