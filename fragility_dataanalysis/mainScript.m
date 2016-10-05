ff12cb52d87a5f2acb03f75642534e472d6b8dbc
Adam <adam2392@gmail.com>
2016-10-05 16:24:56 -0400

    Adding all the computed final files for EZT007 019 090 for row and column perturbations.

--- fragility_dataanalysis/mainScript.m
+++ fragility_dataanalysis/mainScript.m
@@ -13,15 +13,15 @@ timeRange = [60, 0];
 winSize = 500;
 stepSize = 500;
 
-patient_ids = {'007'};
-seizure_ids = {'seiz001', 'seiz002'};
+patient_ids = {'090'};
+seizure_ids = {'seiz003', 'seiz002'};
 % patient_ids = {'005'};
 % seizure_ids = {'seiz001', 'seiz002'};
 
-% patients = { 'JH105sz1', 'pt7sz21', 'pt7sz22', 'pt1sz2', 'pt2sz3'};
+patients = { 'JH105sz1', 'pt7sz21', 'pt7sz22', 'pt1sz2', 'pt2sz3'};
 COMPUTE_ADJ = 0;
 COMPUTE_PERT = 1;
-PLOT = 0;
+PLOT = 1;
 %% Compute Adj Mats for Each Patient and 2 seizures
 if COMPUTE_ADJ
 %     for p=1:length(patients)
