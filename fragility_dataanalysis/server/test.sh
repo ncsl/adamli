b7fed5fa37ecfb54aea7a28973c192c6cb62d9fe
Adam <adam2392@gmail.com>
2016-10-13 13:06:39 -0400

    changing the shell script file to correctly loop through patients.

---
+++ fragility_dataanalysis/server/test.sh
@@ -0,0 +1,14 @@
+# patients listed 5 per row
+patients=('pt1sz2', 'pt1sz3', 'pt2sz1', 'pt2sz3', 'pt7sz19',\
+ 'pt7sz21', 'pt7sz22', 'JH105sz1', 'EZT005seiz001', 'EZT005seiz002', \
+ 'EZT007seiz001','EZT007seiz002', 'EZT019seiz001', 'EZT019seiz002', 'EZT045seiz001',\
+ 'EZT045seiz002','EZT090seiz002', 'EZT090seiz003')
+
+echo ${#patients[@]}
+
+for proc in `seq 0 ${#patients[@]}`; do
+	patient=${patients[proc]}
+	echo $proc
+	echo $patient
+done
+wait
