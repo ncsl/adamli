3459b7117c8f05fbe91eef9edac88622823d9403
Adam <adam2392@gmail.com>
2016-10-19 10:56:27 -0400

    Adding all the figures for freq = 500. Updated saving directories for perturbation analysis to be rerun now with freq=1000, then 500, then 250. Then change window sizes.

--- fragility_dataanalysis/server/runAnalysis.sh
+++ fragility_dataanalysis/server/runAnalysis.sh
@@ -36,9 +36,9 @@ matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
 #  			serverLeastSquaresAdjMat($winIndex, eeg, metadata); exit"
 # done
 
-winSize=500
-stepSize=500
-frequency_sampling=250
+winSize=250
+stepSize=250
+frequency_sampling=1000
 radius=1.1
 
 echo $winSize
