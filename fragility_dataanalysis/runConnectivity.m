53461cfbd6b143a431ffd1a372c4898f11560122
Adam <adam2392@gmail.com>
2016-11-16 16:13:21 -0500

    Adding only 1 node.

--- fragility_dataanalysis/runConnectivity.m
+++ fragility_dataanalysis/runConnectivity.m
@@ -57,6 +57,7 @@ for p=1:length(patients)
     adj_args.seizureStart = seizureStart;
     adj_args.seizureEnd = seizureEnd;
     adj_args.labels = labels;
+    adj_args.l2regularization = l2regularization;
     
     % compute connectivity
     computeConnectivity(patient_id, seizure_id, eeg, clinicalLabels, adj_args);
