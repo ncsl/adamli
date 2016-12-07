6199943c1c6268edd1500e98d62720be8ad8a659
Adam <adam2392@gmail.com>
2016-12-07 11:35:17 -0500

    Fixing main job.sh

--- fragility_dataanalysis/server/main_job.sh
+++ fragility_dataanalysis/server/main_job.sh
@@ -45,7 +45,7 @@ patients=(
 	# pt2sz1 pt2sz3 pt2sz4 pt3sz2 pt3sz4
 	# pt8sz1 pt8sz2 pt8sz3
 	# pt10sz1 pt10sz2 pt10sz3
-	'pt8sz1 pt11sz1 pt11sz2 pt11sz3 pt11sz4 JH107sz7 JH105sz4'
+	'pt8sz1 pt11sz1 pt11sz2 pt11sz3 pt11sz4 JH107sz7 JH105sz4')
 	# pt14sz1 pt14sz2 pt14sz3 pt15sz1 pt15sz2 pt15sz3 pt15sz4
 	# pt16sz1 pt16sz2 pt16sz3 
 	# pt17sz1 pt17sz2
