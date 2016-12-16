1c0c1b0190b98e50d3dc04a550c997b2327075eb
Adam <adam2392@gmail.com>
2016-12-16 11:03:20 -0500

    Testing par script.

--- fragility_dataanalysis/server/runConnectivity.sh
+++ fragility_dataanalysis/server/runConnectivity.sh
@@ -1,3 +1,6 @@
+source /etc/profile.modules
+module load matlab/matlab2013a
+
 ## 00: Load in input parameters
 patient="$1"
 winSize="$2" 
@@ -10,12 +13,12 @@ echo $stepSize
 echo $radius
 echo "Running connectivity computation inside."
 
-# matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
-# [[ ! -z "`which matlab`" ]] || \
-# { 
-# 	echo "MATLAB not found on the PATH; please add to path."; 
-# 	exit 1;
-# }
+matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
+[[ ! -z "`which matlab`" ]] || \
+{ 
+	echo "MATLAB not found on the PATH; please add to path."; 
+	exit 1;
+}
 
 # run perturbation analysis 
 matlab -logfile /home/ali/adamli/fragility_dataanalysis/server/_log/$patient.txt -nojvm -nodisplay -nosplash -r "currentPatient='$patient';\
