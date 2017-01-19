05fc41a85923292f8862d040f1c1c73db38d353f
Adam <adam2392@gmail.com>
2017-01-19 12:48:00 -0500

    Merging.

--- fragility_dataanalysis/server/devVaryingWindows/serverAdjMainScript.m
+++ fragility_dataanalysis/server/devVaryingWindows/serverAdjMainScript.m
@@ -19,12 +19,12 @@ disp(['Looking at patient: ',patient]);
 
 %% New Setup Scripts
 TYPE_CONNECTIVITY = 'leastsquares';     % type of functional conn.?
-BP_FILTER_RAW = 0;                      % apply notch filter before functional conn. computation?
+BP_FILTER_RAW = 1;                      % apply notch filter before functional conn. computation?
 IS_INTERICTAL = 0;                      % is this interictal data?
 l2regularization = 0;                   % apply l2 regularization to estimation of functional conn.?
 
 % set directory to find adjacency matrix data
-toSaveAdjDir = fullfile(strcat('./nofilter_adj_mats_win', num2str(winSize), ...
+toSaveAdjDir = fullfile(strcat('./fixed_adj_mats_win', num2str(winSize), ...
     '_step', num2str(stepSize), '_freq', num2str(frequency_sampling))); % at lab
 dataDir = './data/';
 
