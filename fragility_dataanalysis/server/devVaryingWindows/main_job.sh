05a1a7519cc5185aad44fc6d7af304e0e830d9b0
Adam <adam2392@gmail.com>
2017-01-19 12:31:39 -0500

    Fixed error. Was in pbs file.

--- fragility_dataanalysis/server/devVaryingWindows/main_job.sh
+++ fragility_dataanalysis/server/devVaryingWindows/main_job.sh
@@ -59,10 +59,11 @@ patients=(
 	# JH107sz1 JH107sz2 JH107sz3 JH107sz4 JH107sz5 JH107sz6 JH107sz7 JH107sz8 JH107sz8 JH107sz9
 	# JH108sz1 JH108sz2 JH108sz3 JH108sz4 JH108sz5 JH108sz6 JH108sz7')
 	'EZT019seiz001 EZT019seiz002 EZT019seiz003
-	EZT037seiz001 EZT037seiz002
 	EZT005seiz001 EZT005seiz002 EZT005seiz003
 	EZT007seiz001 EZT007seiz002 EZT007seiz003
 	EZT070seiz001 EZT070seiz002')
+
+# EZT037seiz001 EZT037seiz002
 
 printf "About to run on patients (press enter to continue): $patients" # prompt for patient_id {pt1, pt2, ..., JH105, EZT005}
 read answer
