b3be8bf8201ac6d468951395a668f637b9d9ece8
Adam <adam2392@gmail.com>
2017-01-26 14:49:21 -0500

    Testing parallelized scripts.

--- fragility_dataanalysis/server/devFragility/serverComputeConnectivity.m
+++ fragility_dataanalysis/server/devFragility/serverComputeConnectivity.m
@@ -171,12 +171,14 @@ fprintf(fileName);
 
 % step 2: compute some functional connectivity 
 if strcmp(TYPE_CONNECTIVITY, 'leastsquares')
+    fprintf('About to start least squares');
     % linear model: Ax = b; A\b -> x
     b = double(tempeeg(:)); % define b as vectorized by stacking columns on top of another
     b = b(numChans+1:end); % only get the time points after the first one
 
     % - use least square computation
     theta = computeLeastSquares(tempeeg, b, OPTIONS);
+    fprintf('Finished least squares');
     theta_adj = reshape(theta, numChans, numChans)';    % reshape fills in columns first, so must transpose
 elseif strcmp(TYPE_CONNECTIVITY, 'spearman') || strcmp(TYPE_CONNECTIVITY, 'pearson')
     theta_adj = computePairwiseCorrelation(tmpdata, TYPE_CONNECTIVITY);
