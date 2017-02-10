c57d9e8e876d08ce0b89a937cbcefeb7bd913026
Kriti Jindal <kriti.jindal1@gmail.com>
2017-02-10 08:12:49 -0500

    Added generalized, working DOA function and DOA_script for testing function. Requires necessary .mat files in same folder.

--- fragility_dataanalysis/fragility_library/degreeofagreement/DOA_script.m
+++ fragility_dataanalysis/fragility_library/degreeofagreement/DOA_script.m
@@ -1,9 +1,10 @@
-function [ D ] = DOA( EpiMap, EpiMapStruct, CEZ, ALL, clinicalStruct, threshold )
 % #########################################################################
-% Function Summary: Computes statistic (DOA = Degree of Agreement) indicat-
+% Script for testing function DOA. 
+%
+% Script Summary: Computes statistic (DOA = Degree of Agreement) indicat-
 % ing how well EEZ (from EpiMap) and CEZ (clinical ezone) agree. 
 % 
-% Inputs:
+% Inputs: [function]
 %   EpiMap: Map with keys as electrode labels and corresponding values from
 %   0-1 indicating how likely an electrode is in the ezone 
 %   EpiMapStruct: struct with EpiMap in it
@@ -13,27 +14,40 @@ function [ D ] = DOA( EpiMap, EpiMapStruct, CEZ, ALL, clinicalStruct, threshold 
 %   threshold: value from 0 - 1 required for an electrode in the EpiMap to 
 %   be considered part of the EEZ
 %   
-% Output: 
+% Output: [function]
 %   DOA: (#CEZ intersect EEZ / #CEZ) / (#NOTCEZ intersect EEZ / #NOTCEZ)
 %   Value between -1 and 1, Computes how well CEZ and EEZ match. 
 %   < 0 indicates poor match.
 % 
 % Author: Kriti Jindal, NCSL 
-% Last Updated: 02.09.17
+% Last Updated: 02.10.17
 %   
 % #########################################################################
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% GLOBAL VARIABLES FOR TESTING 
+
+EpiMap = 'fake_data.EpiMap';
+EpiMapStruct = 'fake_data.mat';
+CEZ = 'adjmat_struct.ezone_labels';
+ALL = 'adjmat_struct.all_labels';
+clinicalStruct = 'pt1sz2_adjmats_leastsquares.mat';
+threshold = 0.70;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % load in necesssary structs for EpiMap, CEZ and ALL 
 
 load(EpiMapStruct);
 load(clinicalStruct);
 
-% seperate EpiMap keys and values 
+% seperate EpiMap values and keys 
 
 EpiMap_values = cell2mat(values(EpiMap));
 EpiMap_keys = keys(EpiMap);
 
-% saves all labels in EpiMap with > threshold in vector 'EEZ'
+% saves all labels in EpiMap with > THRESHOLD in vector 'EEZ'
 
 y = 1;
 for x = 1:length(EpiMap_values)
@@ -55,7 +69,3 @@ term2 = length(NotCEZ_EEZ) / length(NotCEZ);
 D = term1 - term2;
 
 fprintf('The degree of agreement with threshold %.2f is %.5f. \n',threshold, D);
-
-
-end
-
