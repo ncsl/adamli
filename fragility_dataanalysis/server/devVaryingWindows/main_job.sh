4b504e0fc3d6244bdbcc1e789284c574eb85937c
Adam <adam2392@gmail.com>
2017-02-22 18:44:32 -0500

    Rerunning on 250 and 125 steps for nih patients.

--- fragility_dataanalysis/server/devVaryingWindows/main_job.sh
+++ fragility_dataanalysis/server/devVaryingWindows/main_job.sh
@@ -5,8 +5,8 @@
 echo "Begin analysis." # print beginning statement
 # printf "Center (nih, cc, jhu): " # prompt for patient_id {pt1, pt2, ..., JH105, EZT005}
 # read center
-printf "Enter window size: " # prompt for seizure_id {sz1, sz2, ..., seiz001, seiz003}
-read winSize
+# printf "Enter window size: " # prompt for seizure_id {sz1, sz2, ..., seiz001, seiz003}
+# read winSize
 # printf "Enter step size: "
 # read stepSize
 # printf "Enter radius: "
@@ -20,13 +20,13 @@ read RUNSLEEP
 
 # patients listed 5 per row
 patients=(
-	# 'pt1aslp1 pt1aslp2 pt1aw1 pt1aw2
-	# pt2aslp1 pt2aslp2 pt2aw1 pt2aw2
-	# pt3aslp1 pt3aslp2 pt3aw1
-	# pt1sz2 pt1sz3 pt1sz4
-	# pt2sz1 pt2sz3 pt2sz4 
-	# pt3sz2 pt3sz4
-	# pt6sz3 pt6sz4 pt6sz5')
+	'pt1aslp1 pt1aslp2 pt1aw1 pt1aw2
+	pt2aslp1 pt2aslp2 pt2aw1 pt2aw2
+	pt3aslp1 pt3aslp2 pt3aw1
+	pt1sz2 pt1sz3 pt1sz4
+	pt2sz1 pt2sz3 pt2sz4 
+	pt3sz2 pt3sz4
+	pt6sz3 pt6sz4 pt6sz5')
 	# 'Pat2sz1p')
 	# Pat2sz2p Pat2sz3p
 	# Pat16sz1p Pat16sz2p Pat16sz3p')
