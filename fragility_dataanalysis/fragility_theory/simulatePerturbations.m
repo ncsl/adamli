2a348487fbbfb956833fd59c66b1e798a06e8b4e
Adam <adam2392@gmail.com>
2017-03-11 14:45:14 -0500

    Adding directory and initial commit to test the perturbation theory of real vs complex eigenvalues.

---
+++ fragility_dataanalysis/fragility_theory/simulatePerturbations.m
@@ -0,0 +1,7 @@
+% script to test fragility theory
+% 1. Create a 3x3, 4x4 matrix with 1 real eigenvalue and 2 complex
+% conjugate eigenvalues
+%
+% 2. Compute minimum norm perturbation of real eigenvalue and complex
+%
+% 3. Show the minimum norm perturbation
