ab4a0ed46f830b5b4a52b024b984aacfcc14122f
Adam <adam2392@gmail.com>
2017-03-16 20:16:48 -0400

    Adding simulations of perturbation matrix.

--- fragility_dataanalysis/fragility_theory/simulatePerturbations.m
+++ fragility_dataanalysis/fragility_theory/simulatePerturbations.m
@@ -5,6 +5,7 @@
 % 2. Compute minimum norm perturbation of real eigenvalue and complex
 %
 % 3. Show the minimum norm perturbation
+close all;
 
 %% add paths/library
 addpath(genpath('../fragility_library/'));
@@ -16,7 +17,8 @@ A = randn(3,3);
 % perform QR factorization to get an orthogonal matrix
 [Q, ~] = qr(A);
 
-L = diag([1, 0.9+1i, 0.9-1i]);
+conjev = sqrt(1-0.5^2);
+L = diag([1, conjev+0.5i, conjev-0.5i]);
 adjMat = Q*L*inv(Q);
 
 radius = 1.5;
@@ -24,14 +26,28 @@ pertArgs.perturbationType = 'C';
 pertArgs.w_space = linspace(-radius, radius, 101);
 pertArgs.radius = radius;
 
+perturbationType = pertArgs.perturbationType;
+w_space = pertArgs.w_space;
+radius = pertArgs.radius;
+
+sigma = sqrt(radius^2 - w_space.^2); % move to the unit circle 1, for a plethora of different radial frequencies
+b = [0; -1];                          % initialize for perturbation computation later
 %% Perform Algorithm
 %%- Compute Minimum Norm Perturbation
+[N, ~] = size(adjMat);
+
+% add to sigma and w to create a whole circle search
+w_space = [w_space, w_space];
+sigma = [-sigma, sigma];
+minPerturbation = zeros(N,1); % initialize minPerturbation Matrix
+
 % store min delta for each electrode X w
 del_size = zeros(N, length(w_space));   % store min_norms
 del_table = cell(N, 1);                 % store min_norm vector for each node
 
 %%- grid search over sigma and w for each row to determine, what is
 %%- the min norm perturbation
+A = adjMat;
 for iNode=1:N % 1st loop through each electrode
     ek = [zeros(iNode-1, 1); 1; zeros(N-iNode,1)]; % unit column vector at this node
    
@@ -79,7 +95,7 @@ for iNode=1:N % 1st loop through each electrode
 %             try
 %                 temp = del*ek';
 %             catch e
-% %                 disp(e)
+%                 disp(e)
 %                 temp = del'*ek'; 
 %             end
 %         else
@@ -87,9 +103,9 @@ for iNode=1:N % 1st loop through each electrode
 %         end
 %         test = A + temp;
 %         plot(real(eig(test)), imag(eig(test)), 'ko')
-% %         if isempty(find(abs(radius - abs(eig(test))) < 1e-8))
-% %             disp('Max eigenvalue is not displaced to correct location')
-% %         end
+%         if isempty(find(abs(radius - abs(eig(test))) < 1e-8))
+%             disp('Max eigenvalue is not displaced to correct location')
+%         end
 %         close all
     end
 
@@ -99,7 +115,7 @@ for iNode=1:N % 1st loop through each electrode
     
     if length(min_index) == 1
         % store the min-norm perturbation vector for this node
-        del_table(iNode) = del_vecs(min_index);
+        del_table(iNode) = {reshape(del_vecs{min_index}, N, 1)};
     else
         temp = del_vecs(min_index);
 
@@ -136,4 +152,41 @@ for iNode=1:N % 1st loop through each electrode
 end % end of loop through channels\
 
 %% Plotting
+figure;
+plot(minPerturbation); hold on;
+
+figure;
+for chan=1:N
+    ek = [zeros(chan-1, 1); 1; zeros(N-chan,1)]; % unit column vector at this node
+    del = del_table{chan};
+    if size(del, 2) == 1 || size(del, 1) == 1
+        del = reshape(del, N, 1);
+        temp = del*ek';
+        test = adjMat + temp;
+        a =plot(real(eig(test)), imag(eig(test)), 'r*', 'MarkerSize', 5); hold on;
+    else
+        for i=1:size(del,2)
+            del_temp = reshape(squeeze(del(:,i)), N, 1);
+            temp = del_temp*ek';
+            test = adjMat + temp;
+            plot(real(eig(test)), imag(eig(test)), 'g*', 'MarkerSize', 5); hold on;
+        end
+    end
+end 
+b= plot(real(eig(adjMat)), imag(eig(adjMat)), 'k*'); hold on;
+legend([a, b], 'Perturbed', 'Original')
+axes = gca;
+xlabelStr = 'Real Part';
+ylabelStr = 'Imag Part';
+titleStr = ['Eigenspectrum of ', perturbationType, ' Perturbation'];
+labelBasicAxes(axes, titleStr, ylabelStr, xlabelStr, FONTSIZE);
+xlim([-radius radius]);
+ylim([-radius radius]);
+plot(get(axes, 'XLim'), [0 0], 'k');
+plot([0 0], get(axes, 'YLim'), 'k');
+th = 0:pi/50:2*pi;
+r = 1; x = 0; y = 0;
+xunit = r * cos(th) + x;
+yunit = r * sin(th) + y;
+h = plot(xunit, yunit);
 
