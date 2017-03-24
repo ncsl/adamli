9ac21e0a8b8f04749373eff23e97ea029bcd5019
Adam <adam2392@gmail.com>
2017-03-24 11:31:58 -0400

    Running fourier analysis on all patients.

--- fragility_dataanalysis/server/devVaryingWindows/preProcess/preprocess_jobs.sh
+++ fragility_dataanalysis/server/devVaryingWindows/preProcess/preprocess_jobs.sh
@@ -13,36 +13,35 @@ read RUNSLEEP
 
 # patients listed 5 per row
 patients=(
-	# 'pt1aslp1 pt1aslp2 pt1aw1 pt1aw2
-	# pt2aslp1 pt2aslp2 pt2aw1 pt2aw2
-	# pt3aslp1 pt3aslp2 pt3aw1
-	# pt1sz2 pt1sz3 pt1sz4
-	# pt2sz1 pt2sz3 pt2sz4 
-	# pt3sz2 pt3sz4
-	# pt6sz3 pt6sz4 pt6sz5
-	# UMMC001_sz1 UMMC001_sz2 UMMC001_sz3
-	# UMMC002_sz1 UMMC002_sz2 UMMC002_sz3
-	# UMMC003_sz1 UMMC003_sz2 UMMC003_sz3
-	# UMMC004_sz1 UMMC004_sz2 UMMC004_sz3
-	# UMMC005_sz1 UMMC005_sz2 UMMC005_sz3
-	# UMMC006_sz1 UMMC006_sz2 UMMC006_sz3
-	# UMMC007_sz1 UMMC007_sz2 UMMC007_sz3
-	# UMMC008_sz1 UMMC008_sz2 UMMC008_sz3
-	# UMMC009_sz1 UMMC009_sz2 UMMC009_sz3
-	# pt7sz19 pt7sz21 pt7sz22
-	# pt8sz1 pt8sz2 pt8sz3
-	# pt10sz1 pt10sz2 pt10sz3
-	# pt11sz1 pt11sz2 pt11sz3 pt11sz4
-	# pt12sz1 pt12sz2
-	# pt13sz1 pt13sz2 pt13sz3 pt13sz5
-	# pt14sz1 pt14sz2 pt14sz3
-	# pt15sz1 pt15sz2 
-	# pt15sz3 pt15sz4
-	# pt16sz1 pt16sz2 pt16sz3
-	# pt17sz1 pt17sz2
-	# EZT004seiz001 EZT004seiz002
-	# EZT005seiz001 EZT005seiz002 
-	'EZT005seiz003
+	'pt1aslp1 pt1aslp2 pt1aw1 pt1aw2
+	pt2aslp1 pt2aslp2 pt2aw1 pt2aw2
+	pt3aslp1 pt3aslp2 pt3aw1
+	pt1sz2 pt1sz3 pt1sz4
+	pt2sz1 pt2sz3 pt2sz4 
+	pt3sz2 pt3sz4
+	pt6sz3 pt6sz4 pt6sz5
+	UMMC001_sz1 UMMC001_sz2 UMMC001_sz3
+	UMMC002_sz1 UMMC002_sz2 UMMC002_sz3
+	UMMC003_sz1 UMMC003_sz2 UMMC003_sz3
+	UMMC004_sz1 UMMC004_sz2 UMMC004_sz3
+	UMMC005_sz1 UMMC005_sz2 UMMC005_sz3
+	UMMC006_sz1 UMMC006_sz2 UMMC006_sz3
+	UMMC007_sz1 UMMC007_sz2 UMMC007_sz3
+	UMMC008_sz1 UMMC008_sz2 UMMC008_sz3
+	UMMC009_sz1 UMMC009_sz2 UMMC009_sz3
+	pt7sz19 pt7sz21 pt7sz22
+	pt8sz1 pt8sz2 pt8sz3
+	pt10sz1 pt10sz2 pt10sz3
+	pt11sz1 pt11sz2 pt11sz3 pt11sz4
+	pt12sz1 pt12sz2
+	pt13sz1 pt13sz2 pt13sz3 pt13sz5
+	pt14sz1 pt14sz2 pt14sz3
+	pt15sz1 pt15sz2 
+	pt15sz3 pt15sz4
+	pt16sz1 pt16sz2 pt16sz3
+	pt17sz1 pt17sz2
+	EZT004seiz001 EZT004seiz002
+	EZT005seiz001 EZT005seiz002 EZT005seiz003
 	EZT006seiz001 EZT006seiz002
 	EZT007seiz001 EZT007seiz002 EZT007seiz003
 	EZT008seiz001 EZT008seiz002
