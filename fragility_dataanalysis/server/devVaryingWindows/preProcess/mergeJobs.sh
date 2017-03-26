e3c1e30a50daa485684f12333e0e465a9d2882bb
Adam <adam2392@gmail.com>
2017-03-26 16:01:44 -0400

    Fixing the merging of jobs for spectral computations.

--- fragility_dataanalysis/server/devVaryingWindows/preProcess/mergeJobs.sh
+++ fragility_dataanalysis/server/devVaryingWindows/preProcess/mergeJobs.sh
@@ -8,8 +8,7 @@ proc="$1"
 patient="$2"
 winSize="$3"
 stepSize="$4"
-currentChan="$5"
-typeTransform="$6"
+typeTransform="$5"
 
 
 ## 01: Set parameters for matlab to run, and check if matlab is on path
@@ -23,4 +22,4 @@ matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
 # run spectral merge
 echo "Running spectral merge..."
 matlab -logfile /home/ali/adamli/fragility_dataanalysis/server/devVaryingWindows/_log/job$1.txt -nojvm -nodisplay -nosplash -r "currentpatient='$patient'; \
-		mergeChannelSpectrum(currentpatient, $winSize, $stepSize, '$typeTransform', $currentChan);"
+		mergeChannelSpectrum(currentpatient, $winSize, $stepSize, '$typeTransform');"
