4e25baccb3a5a84f613cfb9103bd67e6c1572cfe
Adam Li <adam2392@gmail.com>
2017-04-03 14:17:49 -0400

    Adding the merging of connectivity.

---
+++ fragility_dataanalysis/server/devVaryingWindows/mergeComputes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+# This script runs the computation/estimation of adjacency matrices
+source /etc/profile.modules
+module load matlab/matlab2013a
+
+## 00: Load in input parameters
+proc="$1"
+patient="$2"
+winSize="$3"
+stepSize="$4"
+radius="$5"
+RUNCONNECTIVITY="$6"
+
+## 01: Set parameters for matlab to run, and check if matlab is on path
+matlab_jvm="matlab -nojvm -nodesktop -nosplash -r"
+[[ ! -z "`which matlab`" ]] || \
+{ 
+	echo "MATLAB not found on the PATH; please add to path."; 
+	exit 1;
+}
+
+# run adjacency/perturbation merge
+if [[ "$RUNCONNECTIVITY" -eq 1 ]]; then
+	echo "Running connectivity computation."
+	matlab -logfile /home/ali/adamli/fragility_dataanalysis/server/devVaryingWindows/_log/$2_merge_job$1.txt -nojvm -nodisplay -nosplash -r "currentpatient='$patient'; \
+		mergeComputeConnectivity(currentpatient, $winSize, $stepSize);"
+else
+	echo "Running perturbation computation."
+	# run perturbation analysis
+	matlab -logfile /home/ali/adamli/fragility_dataanalysis/server/devVaryingWindows/_log/job$1.txt -nojvm -nodisplay -nosplash -r "currentpatient='$patient'; \
+		serverPerturbationScript(currentpatient, $radius, $winSize, $stepSize);"
+fi
