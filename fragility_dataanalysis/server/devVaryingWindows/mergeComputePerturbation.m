fee32718368aec407323a123c075f0bfc4e053fb
Adam <adam2392@gmail.com>
2017-04-11 12:42:24 -0400

    Fixing the merging of parallelized computation of perturbation matrices.

--- fragility_dataanalysis/server/devVaryingWindows/mergeComputePerturbation.m
+++ fragility_dataanalysis/server/devVaryingWindows/mergeComputePerturbation.m
@@ -75,13 +75,11 @@ if ~exist(toSaveDir, 'dir')
 end
 toSaveDir
 
-
-winsComputed = zeros(N, 1);  
-
 % construct the adjMats from the windows computed of adjMat
 for iMat=1:length(matFileNames)
     matFile = fullfile(tempDir, matFileNames{iMat});
     data = load(matFile);
+    perturbation = data.perturbation_struct;
 
     % check window numbers and make sure they are being stored in order
     currentFile = matFileNames{iMat};
@@ -93,49 +91,30 @@ for iMat=1:length(matFileNames)
     end
     winsComputed(str2num(index)) = 1;
 
-    % initialize matrix if first loop and then store results
-    if iMat==1
-        N = size(theta_adj, 1);
-        adjMats = zeros(length(matFileNames), N, N); 
-    end
-    
     for iPert=1:length(perturbationTypes)
-         % extract the computed tehta adjacency
-        del_table = data.perturbation_struct.del_table;
-        perturbation_struct.(perturbationType).minNormPertMat = minNormPerturbMat;
-        perturbation_struct.(perturbationType).fragilityMat = fragilityMat;
-    end
-        
+        % initialize matrix if first loop and then store results
+        if iMat==1
+            N = size(perturbation.fragilityMat, 1);
+            winsComputed = zeros(N, 1);  
 
-    adjMats(iMat, :, :) = theta_adj;
+            %- initialize
+            perturbation_struct.(perturbationType).minNormPerturbMat = zeros(N, length(matFileNames));
+            perturbation_struct.(perturbationType).fragilityMat = zeros(N, length(matFileNames));
+            perturbation_struct.(perturbationType).del_table = cell(N, length(matFileNames));
+        end
+
+         % extract the computed tehta adjacency
+        perturbation_struct.(perturbationType).del_table(:, iMat) = perturbation.(perturbationType).del_table;
+        perturbation_struct.(perturbationType).minNormPertMat(:, iMat) = perturbation.(perturbationType).minNormPerturbMat;
+        perturbation_struct.(perturbationType).fragilityMat(:, iMat) = perturbation.(perturbationType).fragilityMat;
+    end
 end
 
 %%- Create the structure for the adjacency matrices for this patient/seizure
-adjmat_struct = struct();
-adjmat_struct.type_connectivity = info.type_connectivity;
-adjmat_struct.ezone_labels = info.ezone_labels;
-adjmat_struct.earlyspread_labels = info.earlyspread_labels;
-adjmat_struct.latespread_labels = info.latespread_labels;
-adjmat_struct.resection_labels = info.resection_labels;
-adjmat_struct.all_labels = info.all_labels;
-adjmat_struct.seizure_estart_ms = info.seizure_estart_ms;       % store in ms
-adjmat_struct.seizure_eend_ms = info.seizure_eend_ms;
-adjmat_struct.seizure_cstart_ms = info.seizure_cstart_ms;
-adjmat_struct.seizure_cend_ms = info.seizure_coffset_ms;
-adjmat_struct.seizure_estart_mark = info.seizure_estart_mark;
-adjmat_struct.seizure_eend_mark = info.seizure_eend_mark;
-adjmat_struct.engelscore = info.engelscore;
-adjmat_struct.outcome = info.outcome;
-adjmat_struct.winSize = info.winSize;
-adjmat_struct.stepSize = info.stepSize;
-adjmat_struct.timePoints = info.timePoints;
-adjmat_struct.adjMats = adjMats;
-adjmat_struct.included_channels = info.included_channels;
-adjmat_struct.frequency_sampling = info.frequency_sampling;
-adjmat_struct.FILTER = FILTERTYPE;
+perturbation_struct.info = perturbation.info;
 
 % save the merged adjMatDir
-fileName = strcat(patient, '_adjmats_', lower(info.type_connectivity), '.mat');
+fileName = strcat(patient, '_pertmats_', lower(info.type_connectivity), '_radius', num2str(radius), '.mat');
 
 test = find(winsComputed == 0);
 if isempty(test)
@@ -148,10 +127,10 @@ SUCCESS = 1;
 % Check if it was successful full computation
 if SUCCESS
     try
-        save(fullfile(toSaveDir, fileName), 'adjmat_struct');
+        save(fullfile(toSaveDir, fileName), 'perturbation_struct');
     catch e
         disp(e);
-        save(fullfile(toSaveDir, fileName), 'adjmat_struct', '-v7.3');
+        save(fullfile(toSaveDir, fileName), 'perturbation_struct', '-v7.3');
     end
 
     rmdir(fullfile(tempDir));
