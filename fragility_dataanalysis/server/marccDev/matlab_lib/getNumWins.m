b90fabbf5753923c420c9f0feba004570e71aa39
Adam <adam2392@gmail.com>
2017-05-31 19:13:02 -0400

    Adding changes to see if marcc will work.

--- fragility_dataanalysis/server/marccDev/matlab_lib/getNumWins.m
+++ fragility_dataanalysis/server/marccDev/matlab_lib/getNumWins.m
@@ -1,14 +1,15 @@
 function numWins = getNumWins(patient, winSize, stepSize)
-%% set working directory
-    % data directories to save data into - choose one
-    eegRootDirServer = '/home/ali/adamli/fragility_dataanalysis/';     % work
-    % eegRootDirHome = '/Users/adam2392/Documents/MATLAB/Johns Hopkins/NINDS_Rotation';  % home
-    eegRootDirHome = '/Volumes/NIL_PASS/';
-    eegRootDirJhu = '/home/WIN/ali39/Documents/adamli/fragility_dataanalysis/';
+    %% set working directory
+    eegRootDirServer = '/home/ali/adamli/fragility_dataanalysis/';                 % at ICM server 
+    eegRootDirHome = '/Users/adam2392/Documents/adamli/fragility_dataanalysis/';   % at home macbook
+    eegRootDirJhu = '/home/WIN/ali39/Documents/adamli/fragility_dataanalysis/';    % at JHU workstation
+    eegRootDirMarcc = '/home-1/ali39@jhu.edu/work/adamli/fragility_dataanalysis/'; % at MARCC server
+    % eegRootDirMarcc = '/scratch/groups/ssarma2/adamli/fragility_dataanalysis/';
     % Determine which directory we're working with automatically
     if     ~isempty(dir(eegRootDirServer)), rootDir = eegRootDirServer;
     elseif ~isempty(dir(eegRootDirHome)), rootDir = eegRootDirHome;
     elseif ~isempty(dir(eegRootDirJhu)), rootDir = eegRootDirJhu;
+    elseif ~isempty(dir(eegRootDirMarcc)), rootDir = eegRootDirMarcc;
     else   error('Neither Work nor Home EEG directories exist! Exiting'); end
 
     addpath(genpath(fullfile(rootDir, '/fragility_library/')));
