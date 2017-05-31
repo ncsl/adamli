2d5a69ca6d70e720629febb063f1ec5c223f4ee2
Adam <adam2392@gmail.com>
2017-05-31 17:23:02 -0400

    Testing MARCC for job submissions.

--- fragility_dataanalysis/server/marccDev/matlab lib/parallelComputeConnectivity.m
+++ fragility_dataanalysis/server/marccDev/matlab lib/parallelComputeConnectivity.m
@@ -24,10 +24,12 @@ eegRootDirServer = '/home/ali/adamli/fragility_dataanalysis/';     % work
 % eegRootDirHome = '/Users/adam2392/Documents/MATLAB/Johns Hopkins/NINDS_Rotation';  % home
 eegRootDirHome = '/Users/adam2392/Documents/adamli/fragility_dataanalysis/';
 eegRootDirJhu = '/home/WIN/ali39/Documents/adamli/fragility_dataanalysis/';
+eegRootDirMarcc = '/home-1/ali39@jhu.edu/work/adamli/fragility_dataanalysis/';
 % Determine which directory we're working with automatically
 if     ~isempty(dir(eegRootDirServer)), rootDir = eegRootDirServer;
 elseif ~isempty(dir(eegRootDirHome)), rootDir = eegRootDirHome;
 elseif ~isempty(dir(eegRootDirJhu)), rootDir = eegRootDirJhu;
+elseif ~isempty(dir(eegRootDirMarcc)), rootDir = eegRootDirMarcc;
 else   error('Neither Work nor Home EEG directories exist! Exiting'); end
 
 addpath(genpath(fullfile(rootDir, '/fragility_library/')));
