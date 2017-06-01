2542024a2e9b2bd94986d1338851a7c18705e46e
Adam Li <adam2392@gmail.com>
2017-06-01 10:23:45 -0400

    Fixing patient import for sbatch file. And now has working parallel computing connectivity job submission. git push

--- fragility_dataanalysis/server/marccDev/submit_jobs.sh
+++ fragility_dataanalysis/server/marccDev/submit_jobs.sh
@@ -100,4 +100,4 @@ module load matlab
 # call matlab function to generate slurm files per patient
 matlab -logfile /home-1/ali39@jhu.edu/work/adamli/fragility_dataanalysis/server/marccDev/_log/job$1.txt -nojvm -nodisplay -nosplash -r "\
 	generate_slurm('$patients', $winSize, $stepSize, $radius,\
-	'$partition', '$walltime', $NUM_NODES, $NUM_PROCSPERNODE, $RUNCONNECTIVITY, '$qos');"
+	'$partition', '$walltime', $NUM_NODES, $NUM_PROCSPERNODE, $RUNCONNECTIVITY, '$qos'); exit"
