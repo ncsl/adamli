ecd6d3a0e22f208551a9c5b8e73659830110a0bd
Adam Li <adam2392@gmail.com>
2017-07-05 00:27:52 -0500

    Changed testing jobs on laser ablation patients to debug partition.

--- fragility_dataanalysis/server/marccDev/submit_jobs.sh
+++ fragility_dataanalysis/server/marccDev/submit_jobs.sh
@@ -96,7 +96,7 @@ if [[ "${RUNCONNECTIVITY}" -eq 1 ]]; then
 else
 	walltime=0:20:0					# the walltime for each computation
 fi
-partition=scavenger 	# debug, shared, unlimited, parallel, gpu, lrgmem, scavenger
+partition=debug 	# debug, shared, unlimited, parallel, gpu, lrgmem, scavenger
 qos=scavenger
 
 ## load in the modules for this run -> python, matlab, etc.
