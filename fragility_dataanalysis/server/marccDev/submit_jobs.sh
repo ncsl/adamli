b901a39ba3e9d9ed10be1ab69a234a1957fc2f1b
Adam Li <adam2392@gmail.com>
2017-09-25 00:38:59 +0200

    Resubmitting jobs for just a few nih patients to run through common avg referencing.

--- fragility_dataanalysis/server/marccDev/submit_jobs.sh
+++ fragility_dataanalysis/server/marccDev/submit_jobs.sh
@@ -135,7 +135,7 @@ echo $reference
 matlab -logfile /home-1/ali39@jhu.edu/work/adamli/fragility_dataanalysis/server/marccDev/_log/job$1.txt -nojvm -nodisplay -nosplash -r "\
 	generate_slurm('$buff', $winSize, $stepSize, $radius,\
 	'$partition', '$walltime', $NUM_NODES, $NUM_PROCSPERNODE,\
-	 $RUNCONNECTIVITY, $reference); exit"
+	 $RUNCONNECTIVITY, '$reference'); exit"
 
 ## 02: Call patient shell script for each patient
 # matlab -logfile /home-1/ali39@jhu.edu/work/adamli/fragility_dataanalysis/server/marccDev/_log/job$1.txt -nojvm -nodisplay -nosplash -r "\
