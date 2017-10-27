3061fb05876fefc9a72b769ec5a54ef4489998e2
Adam Li <adam2392@gmail.com>
2017-10-27 15:31:32 +0200

    Fixing generation slurm gnu merging.

--- fragility_dataanalysis/fragility_theory/smallScalePerturbations.m
+++ fragility_dataanalysis/fragility_theory/smallScalePerturbations.m
@@ -27,7 +27,9 @@ addpath(rootDir);
 center = 'nih';
 patient = 'pt7sz19';
 center='cc';
-patient = 'EZT019seiz001';
+patient = 'pt17sz1';
+patient = 'pt1aw1';
+patient = 'pt1aslp1';
 center = 'nih';
 patient = 'pt1sz2';
 
@@ -43,7 +45,7 @@ patient = 'pt1sz2';
 dataDir = fullfile(dataDir, 'data', center);
 
 % plotting options
-FONTSIZE = 16;
+% FONTSIZE = 16;
 figDir = fullfile(rootDir, 'fragility_theory', 'figures');
 if ~exist(figDir, 'dir')
     mkdir(figDir);
@@ -104,7 +106,9 @@ numSampsInWin = winSize * frequency_sampling / 1000;
 numSampsInStep = stepSize * frequency_sampling / 1000;
 numWins = floor(size(eeg, 2) / numSampsInStep - numSampsInWin/numSampsInStep + 1);
 
-P = 3; % size of simulation
+eeg = eeg(included_channels, :);
+
+P = 20; % size of simulation
 numSims = 200;
 all_del_sizes = zeros(numSims*P, length(w_space)); % store all min del vectors for all wspace
 all_adjmats = zeros(numSims, P, P);                 % store all adjmats used
@@ -152,72 +156,132 @@ list_allfreqs = [all_del_freqs{:}];
 avg_minnorm = nanmean(all_del_sizes, 1);
 var_minnorm = nanvar(all_del_sizes, [], 1);
 
-%% figure to show along wspace waht the min norm was at each point
-figure;
+% get rid of all 'unrealistic' perturbations that are too large
+% bad_indices = find(avg_minnorm > 1000000);
+% avg_minnorm(bad_indices) = nan;
+% var_minnorm(bad_indices) = nan;
+
+%% figure to show along wspace what the min norm was at each point
+FONTSIZE = 20;
+
+fig = figure;
 shadedErrorBar(1:length(w_space), avg_minnorm, var_minnorm, 'ko');
 axes = gca;
-title('Distribution of Minimum Norm Perturbation Along Stability Radius');
+axes.Box = 'off';
+axes.FontSize = FONTSIZE;
+hold on;
+plot([26, 26], axes.YLim, 'r-');
+plot([76, 76], axes.YLim, 'r-');
+title('Distribution of Minimum Norm Perturbation Along Stability Radius', 'FontSize', FONTSIZE);
 xlabel('W-space Index');
 ylabel('Euclidean Norm of Perturbation');
 
+pause(0.05);
+legend('Delta Norms', 'Imaginary part = 0', 'Location', 'southeast');
+set(fig, 'Units', 'inches');
+fig.Position = [0.0417 0.6667 21.0694 13.0139];
+
+toSaveFigFile = fullfile(figDir, strcat(patient, '_numchans', num2str(P), '_deltadistribution'));
+print(toSaveFigFile, '-dpng', '-r0');
+
+
 %% figure to show the distribution of average min norms
-mindel_overw = min(all_del_sizes);
+[mindel_overw, min_indices] = min(all_del_sizes);
+[maxdel_overw, max_indices] = max(all_del_sizes);
 
 xticklabs = {};
 for i=1:length(w_space)
-    xstr = strcat(num2str(sigma(i)), '+', num2str(w_space(i)), 'j');
+    xstr = strcat(num2str(round(sigma(i), 2)), '+', num2str(round(w_space(i),2)), 'j');
     xticklabs{end+1} = xstr;
 end
 
-figure;
+fig = figure;
+indices_to_plot = 60:80;
+zero_indices = [26, 76];
+
 subplot(211); % plot over entire instability radius
 plot(1:length(w_space), min(all_del_sizes), 'ko');
 xlim([1 length(w_space)]);
 axes = gca;
+axes.FontSize = FONTSIZE;
 xticklocs = axes.XTick;
 axes.XTickLabel = xticklabs(xticklocs);
+axes.Box = 'off';
 hold on;
 plot([26, 26], axes.YLim, 'r-');
 plot([76, 76], axes.YLim, 'r-');
-title('Minimum Norm At Each Point Along Stability Radius');
-ylabel('Minimum Norm');
+plot([indices_to_plot(1), indices_to_plot(1)], axes.YLim, 'g--');
+plot([indices_to_plot(end) indices_to_plot(end)], axes.YLim, 'g--');
+title('Minimum Norm At Each Point Along Stability Radius', 'FontSize', FONTSIZE);
+ylab1 = ylabel('Minimum Norm');
+ylab1.Position = ylab1.Position + [-3, 0 , 0];
 
 subplot(212); % plot over area around w = 0
-indices_to_plot = 60:80;
 plot(indices_to_plot, mindel_overw(indices_to_plot), 'ko');
 axes = gca;
-% axes.XTickLabel = xticklabs(indices_to_plot);
+axes.FontSize = FONTSIZE;
+axes.XTickLabel = xticklabs(indices_to_plot);
+axes.Box = 'off';
 hold on;
-title('Minimum Norm At Each Point Along Stability Radius');
-ylabel('Minimum Norm');
+plot([76, 76], axes.YLim, 'r-');
+title('Minimum Norm At Each Point Along Stability Radius', 'FontSize', FONTSIZE);
+ylab2 = ylabel('Minimum Norm');
+% ylab2.Position = ylab2.Position + [-3, 0 , 0];
+
+pause(0.05);
+legend('Delta Norms', 'Imaginary part = 0', 'Location', 'southeast');
+set(fig, 'Units', 'inches');
+% fig_heatmap.Position = [17.3438         0   15.9896   11.6771];
+fig.Position = [0.0417 0.6667 21.0694 13.0139];
+
+toSaveFigFile = fullfile(figDir, strcat(patient, '_numchans', num2str(P), '_realdata_randomtime'));
+print(toSaveFigFile, '-dpng', '-r0');
+
+%% Figure of Eigenspectrum of A matrix at minimum/maximum minperturbation
+% add subplot of histogram of time of simulation that this occurred
+% subplot(311);
+% hist(min_indices);
+
+fig = figure;
+subplot(221);
+min_index_forA = ceil(min_indices(zero_indices(2)) / 3);
+min_adjmat = squeeze(all_adjmats(min_index_forA, :, :));
+imagesc(min_adjmat); colorbar(); colormap('jet');
+ax = gca;
+hold on; axis tight;
+set(ax, 'box', 'off'); set(ax, 'YDir', 'normal');
+
+subplot(223);
+plot(real(eig(min_adjmat)), imag(eig(min_adjmat)), 'b*', 'MarkerSize', 5); hold on;
+axes = gca;
+xlabelStr = 'Real Part';
+ylabelStr = 'Imag Part';
+titleStr = ['Eigenspectrum of ', perturbationType, ' Perturbation'];
+labelBasicAxes(axes, titleStr, ylabelStr, xlabelStr, FONTSIZE);
+% xlim([-radius radius]);
+% ylim([-radius radius]);
+% plot(get(axes, 'XLim'), [0 0], 'k');
+% plot([0 0], get(axes, 'YLim'), 'k');
+% th = 0:pi/50:2*pi;
+% r = 1; x = 0; y = 0;
+% xunit = r * cos(th) + x;
+% yunit = r * sin(th) + y;
+% h = plot(xunit, yunit);
+
+subplot(222);
+max_index_forA = ceil(max_indices(zero_indices(2)) / 3);
+max_adjmat = squeeze(all_adjmats(max_index_forA, :, :));
+imagesc(max_adjmat); colorbar(); colormap('jet');
+ax = gca;
+hold on; axis tight;
+set(ax, 'box', 'off'); set(ax, 'YDir', 'normal');
+
+subplot(224);
+plot(real(eig(max_adjmat)), imag(eig(max_adjmat)), 'b*', 'MarkerSize', 5); hold on;
+axes = gca;
+xlabelStr = 'Real Part';
+ylabelStr = 'Imag Part';
+titleStr = ['Eigenspectrum of ', perturbationType, ' Perturbation'];
+labelBasicAxes(axes, titleStr, ylabelStr, xlabelStr, FONTSIZE);
 
 
-% colors = {'k', 'b', 'r'};
-% 
-% temp = squeeze(mean(all_del_sizes, 1));
-% tempstd = squeeze(std(all_del_sizes, 0, 1));
-% % figure;
-% for i=1:numChans
-%     figure;
-%     %     shadedErrorBar(1:length(w_space), temp(i,:), tempstd(i,:));
-%     plot(1:length(w_space), temp(i,:), 'Color', colors{i}); hold on;
-%     plot(1:length(w_space), squeeze(min(all_del_sizes(:,i,:))), 'LineStyle', ':', 'Color', colors{i});
-% %     plot(1:length(w_space), squeeze(max(all_del_sizes(:,i,:))), 'LineStyle', '--', 'Color', colors{i});
-%     ax = gca;
-%     ax.FontSize = FONTSIZE;
-%     xlabel('Along W Space');
-%     ylabel('Delta Norms');
-%     title(['Delta Norms over W Space']);
-%     legend('Average Norms', 'Min Norms', 'Max Norms');
-%     
-%     currfig = gcf;
-%     set(currfig, 'Units', 'inches');
-%     currfig.Position = [17.3438         0   15.9896   11.6771];
-%     
-%     toSaveFigFile = fullfile(figDir, strcat(patient, '_chanindex', num2str(i), '_realdata_randomtime'));
-%     print(toSaveFigFile, '-dpng', '-r0');
-%     
-%     close all
-% end
-% 
-% disp('done')
